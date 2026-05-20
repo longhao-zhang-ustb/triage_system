c16d59077cefb461ceac5e14b891f81da7e96b44
Longhao Zhang <zlh_ustb2022@163.com>
2026-05-21 00:25:49 +0800

    Add axios request utility with interceptors

---
+++ front_end/src/utils/request.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const apiBaseURL = '';
+
+const service = axios.create({
+  baseURL: apiBaseURL,
+  timeout: 5000
+});
+
+service.interceptors.request.use(
+  config => config,
+  error => {
+    console.error('Request error:', error);
+    return Promise.reject(error);
+  }
+);
+
+service.interceptors.response.use(
+  response => response.data,
+  error => {
+    console.error('Response error:', error);
+    return Promise.reject(error);
+  }
+);
+
+export default service;
