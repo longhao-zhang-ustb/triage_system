7e36ab55eb4f75cfb6ae960ff7d8c687a99b7fbd
Longhao Zhang <zlh_ustb2022@163.com>
2026-05-21 00:23:45 +0800

    Add routing for Statistics and Manage components

---
+++ front_end/src/router/index.js
@@ -0,0 +1,12 @@
+import Statistics from '@/components/Statistics.vue'
+import Manage from '@/components/Manage.vue'
+const routes = [
+    {
+        path: '/statistics',
+        component: Statistics
+    },
+    {
+        path: '/manage',
+        component: Manage
+    }
+]
