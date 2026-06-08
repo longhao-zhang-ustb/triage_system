414fe5679b4e7f8538c229ea5ec07e392728e73a
Longhao Zhang <zlh_ustb2022@163.com>
2026-06-09 06:31:21 +0800

    Enhance getTriageResult with input validation
    Added error handling for missing input parameters in triage calculations.

--- back_end/intelligenttriage/views.py
+++ back_end/intelligenttriage/views.py
@@ -26,15 +26,31 @@ def getTriageResult(request):
     temp = data.get("temp")
     pulse = data.get("pulse")
     #-----------------------------------------------
+    print(breath, sysbp, gcs, chest, gcs_motor, gcs_verbal, age, hr, temp, pulse)
     # 通过修正创伤计分(Revised Trauma Score, RTS)获取检伤结果
-    RTS_res = getRTSResult(breath, sysbp, gcs)
+    if breath != -1 and sysbp != -1 and gcs != -1:
+        RTS_res = getRTSResult(breath, sysbp, gcs)
+    else:
+        RTS_res = "--"
     # 通过院前指数评估(prehospital index, PHI)获取检伤结果
-    PHI_res = getPHIResult(gcs, gcs_verbal, sysbp, pulse, breath, chest)
+    if gcs != -1 and gcs_verbal != -1 and sysbp != -1 and pulse != -1 and breath != -1 and chest != -1:
+        PHI_res = getPHIResult(gcs, gcs_verbal, sysbp, pulse, breath, chest)
+    else:
+        PHI_res = "--"
     # 通过CRAMS法获取检伤结果
-    CRAMS_res = getCRAMSResult(sysbp, breath, chest, gcs_motor, gcs_verbal)
+    if sysbp != -1 and breath != -1 and chest != -1 and gcs_motor != -1 and gcs_verbal != -1:
+        CRAMS_res = getCRAMSResult(sysbp, breath, chest, gcs_motor, gcs_verbal)
+    else:
+        CRAMS_res = "--"
     # 通过GCS法获取伤员的意识状态
-    GCS_res = getGCSResult(gcs)
+    if gcs != -1:
+        GCS_res = getGCSResult(gcs)
+    else:
+        GCS_res = "--"
     # 通过机器学习模型评估伤员死亡率风险: age, hr, sysbp, temp, gcs
-    ML_res = getMLResult(age, hr, sysbp, temp, gcs)
-    
+    if age != -1 and hr != -1 and sysbp != -1 and temp != -1 and gcs != -1:
+        ML_res = getMLResult(age, hr, sysbp, temp, gcs)
+    else:
+        ML_res = "--_--"
+    print('-----------------', {"RTS": RTS_res, "PHI": PHI_res, "CRAMS": CRAMS_res, "GCS": GCS_res, "ML": ML_res})
     return JsonResponse({"code": 200, "msg": "获取成功", "triage_result": {"RTS": RTS_res, "PHI": PHI_res, "CRAMS": CRAMS_res, "GCS": GCS_res, "ML": ML_res}})
