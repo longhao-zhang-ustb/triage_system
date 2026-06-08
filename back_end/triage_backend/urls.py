7820d7da2b646e39a9c83951ac54144624f26d4e
Longhao Zhang <zlh_ustb2022@163.com>
2026-06-09 06:38:01 +0800

    Enable static file serving for debug mode
    Add static file serving in debug mode.

--- back_end/triage_backend/urls.py
+++ back_end/triage_backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
@@ -25,3 +27,6 @@ urlpatterns = [
     path('api/intelligenttriage/', include('intelligenttriage.urls')),
     path('api/saveInfo/', include('saveInfo.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
